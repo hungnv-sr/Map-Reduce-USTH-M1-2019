0a8c1ef4b9e30ca56aa987715614d9129886f00c
lehuyduc <lehuyduc3@gmail.com>
2020-03-07 22:06:22 +0700

    Change name of load Dataset button (wrong name)

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -607,7 +607,7 @@ void MainWindow::on_pButtonSaveDataset_clicked()
 
 }
 
-void MainWindow::on_pButtonLoadDistribution_clicked()
+void MainWindow::on_pButtonLoadDataset_clicked()
 {
     if (!resource.available()) {
         QMessageBox::information(this, "Error", "Another task is in progress. Please wait");
